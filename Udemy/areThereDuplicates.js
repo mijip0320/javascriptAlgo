853c6cdb24e1056f296a19638e794233401e3bd9
mijip0320 <39548731+mijip0320@users.noreply.github.com>
2025-03-20 07:41:55 +0900

    feat : add algorithm

--- Udemy/areThereDuplicates.js
+++ Udemy/areThereDuplicates.js
@@ -1,6 +1,25 @@
 var areThereDuplicates = function duplicates(...args) {
 //Rest 파라미터를 사용하면 가변 인자 함수의 인수 목록을 배열로 직접 전달 받을 수 있음
-  console.log(args)
+ 
+ 
+  let count = {};
+
+  for (let i = 0; i < args.length; i++){
+    let letter = args[i];
+    if (count[letter]) {
+      count[letter] += 1;
+    } else {
+      count[letter] = 1;
+    }
+  }
+  //console.log(Object.values(count));
+  
+  if (Object.values(count).filter((el) => el !== 1).length > 0) {
+    console.log('true')
+  } else {
+    console.log('false')
+  }
+
 }
 
 //areThereDuplicates(1, 2, 3) // false
---
+++ Udemy/averagePair.js
@@ -0,0 +1,23 @@
+var averagePair = function pair(arr, num) {
+    if (arr.length === 0) console.log('false');
+
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start < end) {
+        let average = (arr[start] + arr[end]) / 2;
+        if (average === num) {
+            console.log('true');
+            return;
+        } else {
+            if (average < num) start++;
+            else end--;
+        }
+    }
+    console.log('false');
+}
+
+ averagePair([1,2,3],2.5) // true
+// averagePair([1,3,3,5,6,7,10,12,19],8) // true
+// averagePair([-1,0,3,4,5,6], 4.1) // false
+// averagePair([],4) // false
