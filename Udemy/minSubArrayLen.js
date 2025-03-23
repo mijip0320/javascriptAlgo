7053de9fe35a8fb07f126a20fe43fcefb6358b00
mijip0320 <39548731+mijip0320@users.noreply.github.com>
2025-03-24 07:52:54 +0900

    feat : solving problem

--- Udemy/minSubArrayLen.js
+++ Udemy/minSubArrayLen.js
@@ -1,4 +1,54 @@
-var minSubArrayLen = function subarray() {
+var minSubArrayLen = function subarray(arr, num) {
+     
+    let maxNum = [...arr].sort((a,b) => a-b)[arr.length - 1];
+
+    if (maxNum >= num) {
+        console.log('1');
+        return;
+    }
+    // console.log(arr.indexOf(maxNum));
+    let maxIndex = arr.indexOf(maxNum);
+
+    let i = maxIndex !== 0 ? maxIndex - 1 : 0;
+    let j = maxIndex;
+    let tempMax =  maxNum;
+    while (i >= 0 && j < arr.length) {
+            
+        if (maxIndex !== 0) {
+            j = j === maxIndex ? maxIndex + 1 : j;
+            
+            
+            if (arr[i] > arr[j]) {
+                tempMax += arr[i];
+            } else {
+                tempMax += arr[j];
+            }
+
+            if (tempMax >= num) {
+                console.log(j - i + 1);
+                return;
+            }
+
+            i--;
+            j++;
+              
+            
+        } else {
+             j = j === 0 ? 1 : j
+            let tempMax = maxNum + arr[j];
+            if (tempMax >= num) {
+                console.log(j);
+                return;
+            }
+             j++;
+        }
+
+        
+
+        
+    }
+
+    //console.log(j-i);
 
 }
 
@@ -6,7 +56,7 @@ var minSubArrayLen = function subarray() {
 // minSubArrayLen([2,1,6,5,4], 9) // 2 -> because [5,4] is the smallest subarray
 // minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52) // 1 -> because [62] is greater than 52
 // minSubArrayLen([1,4,16,22,5,7,8,9,10],39) // 3
-// minSubArrayLen([1,4,16,22,5,7,8,9,10],55) // 5
+ minSubArrayLen([1,4,16,22,5,7,8,9,10],55) // 5
 // minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11) // 2
 // minSubArrayLen([1,4,16,22,5,7,8,9,10],95) // 0
 
