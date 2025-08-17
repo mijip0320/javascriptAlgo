f00242bb076d1eb94957ba4793def066b286f075
mijip0320 <39548731+mijip0320@users.noreply.github.com>
2025-08-18 07:50:16 +0900

    feat : update hacckerank

--- Hackerrank/drawingBook.js
+++ Hackerrank/drawingBook.js
@@ -1,4 +1,8 @@
 //https://www.hackerrank.com/challenges/drawing-book/problem?isFullScreen=true
-var book = function drawingBook(n, p) {};
+var book = function drawingBook(n, p) {
+  const fromFront = Math.floor(p / 2);
+  const fromBack = Math.floor(n / 2) - Math.floor(p / 2);
+  return Math.min(fromFront, fromBack);
+};
 
-console.log(book(5, 3));
+console.log(book(6, 2));
