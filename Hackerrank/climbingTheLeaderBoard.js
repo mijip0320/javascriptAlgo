c1d3b0c7878da02c91653e69a10360672bf8d1c3
mijip0320 <39548731+mijip0320@users.noreply.github.com>
2025-12-19 07:48:00 +0900

    Update climbingTheLeaderBoard.js

--- Hackerrank/climbingTheLeaderBoard.js
+++ Hackerrank/climbingTheLeaderBoard.js
@@ -1,4 +1,24 @@
 //https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem?isFullScreen=true
-var climbLeaderBoard = function climbingLeaderboard(ranked, player) {};
+var climbLeaderBoard = function climbingLeaderboard(ranked, player) {
+  // 1) ranked 중복 제거 + 내림차순 유지
+  const unique = [];
+  for (const s of ranked) {
+    if (unique.length === 0 || unique[unique.length - 1] !== s) {
+      unique.push(s);
+    }
+  }
 
-console.log(climbingLeaderboard([4, 6, 5, 3, 3, 1]));
+  // 2) 투포인터
+  const res = [];
+  let i = unique.length - 1; // 가장 낮은 점수 인덱스
+
+  for (const p of player) {
+    while (i >= 0 && p >= unique[i]) i--;
+    // i가 -1이면 1등, 아니면 i 위치 다음 등수
+    res.push(i + 2);
+  }
+
+  return res;
+};
+
+console.log(climbLeaderBoard([100, 100, 50, 40, 40, 20, 10], [5, 25, 50, 120]));
